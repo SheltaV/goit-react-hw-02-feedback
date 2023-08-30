4307050f8c6641450c492d94f10c8ba7525ebf3e
SheltaV <bluegiraffezaoz@gmail.com>
2023-08-30 20:43:29 +0300

    update-min

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,6 +1,5 @@
-export const Statistics = ({good, neutral, bad, total, positivePercentage}) => {
+export const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
     return <div>
-
         <p>Good: {good}</p>
         <p>Neutral: {neutral}</p>
         <p>Bad: {bad}</p>
